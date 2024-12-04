326f48311d59cf0dd7ddffa31084e90854d01b2c
LnYKLl <lennykiloul@outlook.com>
2024-12-04 02:28:03 +0100

    GNL

--- get_next_line.c
+++ get_next_line.c
@@ -5,24 +5,68 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkiloul <lkiloul@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/20 14:05:21 by lkiloul           #+#    #+#             */
-/*   Updated: 2024/11/25 16:45:34 by lkiloul          ###   ########.fr       */
+/*   Created: 2024/12/04 01:27:31 by lkiloul           #+#    #+#             */
+/*   Updated: 2024/12/04 02:23:21 by lkiloul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-char *get_next_line(int fd)
-{
-    char *buffer;
-    int bytes_read;
 
-    buffer = malloc(sizeof(char *) * BUFFER_SIZE + 1);
-    if (!buffer)
-        return (NULL);
-    bytes_read = read(fd, buffer, BUFFER_SIZE);
-    if (!bytes_read)
-        return (NULL);
-    while (!ft_strchr(buffer, '\n'))
-        ft_putstr(buffer);
-    return ("test");
+static char	*get_line(char **str)
+{
+	char	*line;
+	char	*tmp;
+	size_t	i;
+
+	i = 0;
+	while ((*str)[i] && (*str)[i] != '\n')
+		i++;
+	if ((*str)[i] == '\n')
+	{
+		line = ft_substr(*str, 0, i + 1);
+		tmp = ft_strdup(*str + i + 1);
+		free(*str);
+		*str = tmp;
+		if (**str == '\0')
+		{
+			free(*str);
+			*str = NULL;
+		}
+	}
+	else
+	{
+		line = ft_strdup(*str);
+		free(*str);
+		*str = NULL;
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str;
+	char		buffer[BUFFER_SIZE + 1];
+	char		*tmp;
+	ssize_t		bytes_read;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		buffer[bytes_read] = '\0';
+		if (!str)
+			str = ft_strdup(buffer);
+		else
+		{
+			tmp = ft_strjoin(str, buffer);
+			str = tmp;
+		}
+		if (ft_strchr(str, '\n'))
+			break ;
+	}
+	if (bytes_read < 0)
+		return (NULL);
+	if (bytes_read == 0 && (!str || *str == '\0'))
+		return (NULL);
+	return (get_line(&str));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,28 @@
 /*   By: lkiloul <lkiloul@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 15:51:56 by lkiloul           #+#    #+#             */
-/*   Updated: 2024/11/25 16:46:00 by lkiloul          ###   ########.fr       */
+/*   Updated: 2024/12/04 02:19:49 by lkiloul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GNL_H
-# define GNL_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <fcntl.h>
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-# define BUFFER_SIZE 5
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
 void	ft_putstr(char *str);
 char	*ft_strchr(const char *s, int c);
 char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
