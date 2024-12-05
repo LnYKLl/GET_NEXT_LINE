b5ab0f49130ec87ee4bed3a6ddb0d49555e03b26
LnYKLl <lennykiloul@outlook.com>
2024-12-05 05:37:31 +0100

    Fix norminette header

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lkiloul <lkiloul@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 15:51:56 by lkiloul           #+#    #+#             */
-/*   Updated: 2024/12/04 08:52:47 by lkiloul          ###   ########.fr       */
+/*   Updated: 2024/12/05 04:44:55 by lkiloul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*get_next_line(int fd);
-char *ft_get_line(char *str, ssize_t  bytes_read, int fd, char *buffer);
+char	*ft_get_line(char *str, ssize_t bytes_read, int fd, char *buffer);
 
 #endif
