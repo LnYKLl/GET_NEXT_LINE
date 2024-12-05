899b471fd2bedc29b4817afbae955ecb43bfa39a
LnYKLl <lennykiloul@outlook.com>
2024-12-05 05:01:35 +0100

    fix norminette + fix script

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,19 @@
 /*   By: lkiloul <lkiloul@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 01:27:31 by lkiloul           #+#    #+#             */
-/*   Updated: 2024/12/04 09:05:15 by lkiloul          ###   ########.fr       */
+/*   Updated: 2024/12/05 05:00:53 by lkiloul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static void	ft_free(char **s)
+{
+	if (!*s)
+		return ;
+	free(*s);
+	*s = NULL;
+}
 
 static char	*get_line(char **str)
 {
@@ -28,25 +36,38 @@ static char	*get_line(char **str)
 		free(*str);
 		*str = tmp;
 		if (**str == '\0')
-		{
-			free(*str);
-			*str = NULL;
-		}
+			ft_free(str);
 	}
 	else
 	{
 		line = ft_strdup(*str);
-		free(*str);
-		*str = NULL;
+		ft_free(str);
 	}
 	return (line);
+}
+
+static char	*add_line(char *str, char *buffer, ssize_t bytes_read)
+{
+	char	*tmp;
+
+	buffer[bytes_read] = '\0';
+	if (str == NULL)
+	{
+		str = ft_strdup(buffer);
+	}
+	else
+	{
+		tmp = ft_strjoin(str, buffer);
+		free(str);
+		str = tmp;
+	}
+	return (str);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*str;
 	char		buffer[BUFFER_SIZE + 1];
-	char		*tmp;
 	ssize_t		bytes_read;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
@@ -57,15 +78,7 @@ char	*get_next_line(int fd)
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read < 0)
 			return (NULL);
-		buffer[bytes_read] = '\0';
-		if (!str)
-			str = ft_strdup(buffer);
-		else
-		{
-			tmp = ft_strjoin(str, buffer);
-			free(str);
-			str = tmp;
-		}
+		str = add_line(str, buffer, bytes_read);
 		if (ft_strchr(str, '\n'))
 			break ;
 	}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: lkiloul <lkiloul@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 14:50:11 by lkiloul           #+#    #+#             */
-/*   Updated: 2024/12/04 08:27:02 by lkiloul          ###   ########.fr       */
+/*   Updated: 2024/12/05 04:45:10 by lkiloul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,97 +14,97 @@
 
 static int	ft_strlen(char *s)
 {
-    int	i;
+	int	i;
 
-    i = 0;
-    while (s[i])
-        i++;
-    return (i);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
 char	*ft_strchr(const char *s, int c)
 {
-    int	i;
+	int	i;
 
-    i = 0;
-    while (s[i] != (char)c)
-    {
-        if (s[i] == '\0')
-            return (0);
-        i++;
-    }
-    return ((char *)s + i);
+	i = 0;
+	while (s[i] != (char)c)
+	{
+		if (s[i] == '\0')
+			return (0);
+		i++;
+	}
+	return ((char *)s + i);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-    int				i;
-    int				k;
-    int				j;
-    unsigned char	*t;
+	int				i;
+	int				k;
+	int				j;
+	unsigned char	*t;
 
-    i = ft_strlen((char *)s1);
-    k = ft_strlen((char *)s2);
-    j = 0;
-    t = malloc((i + k + 1) + sizeof(char));
-    if (!t)
-        return (NULL);
-    while (s1[j] != '\0')
-    {
-        t[j] = s1[j];
-        j++;
-    }
-    j = 0;
-    while (s2[j] != '\0')
-    {
-        t[i + j] = s2[j];
-        j++;
-    }
-    t[i + j] = '\0';
-    return ((char *)t);
+	i = ft_strlen((char *)s1);
+	k = ft_strlen((char *)s2);
+	j = 0;
+	t = malloc((i + k + 1) + sizeof(char));
+	if (!t)
+		return (NULL);
+	while (s1[j] != '\0')
+	{
+		t[j] = s1[j];
+		j++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		t[i + j] = s2[j];
+		j++;
+	}
+	t[i + j] = '\0';
+	return ((char *)t);
 }
 
 char	*ft_strdup(const char *s)
 {
-    int		i;
-    char	*t;
+	int		i;
+	char	*t;
 
-    i = ft_strlen((char *)s);
-    t = malloc(i + 1);
-    if (!t)
-        return (NULL);
-    i = 0;
-    while (s[i])
-    {
-        t[i] = s[i];
-        i++;
-    }
-    t[i] = '\0';
-    return (t);
+	i = ft_strlen((char *)s);
+	t = malloc(i + 1);
+	if (!t)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		t[i] = s[i];
+		i++;
+	}
+	t[i] = '\0';
+	return (t);
 }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    unsigned int	i;
-    unsigned int	k;
-    char			*f;
+	unsigned int	i;
+	unsigned int	k;
+	char			*f;
 
-    i = ft_strlen((char *)s);
-    k = 0;
-    if (!s || len == 0 || start > i)
-        return (ft_strdup(""));
-    if ((size_t)start + len > i)
-        len = i - start;
-    f = malloc(len + 1 * sizeof(char));
-    if (!f)
-    {
-        return (NULL);
-    }
-    while ((size_t)k < len)
-    {
-        f[k] = s[start + k];
-        k++;
-    }
-    f[k] = '\0';
-    return (f);
+	i = ft_strlen((char *)s);
+	k = 0;
+	if (!s || len == 0 || start > i)
+		return (ft_strdup(""));
+	if ((size_t)start + len > i)
+		len = i - start;
+	f = malloc(len + 1 * sizeof(char));
+	if (!f)
+	{
+		return (NULL);
+	}
+	while ((size_t)k < len)
+	{
+		f[k] = s[start + k];
+		k++;
+	}
+	f[k] = '\0';
+	return (f);
 }
